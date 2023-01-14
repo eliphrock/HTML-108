ddfe19ca1c460494eb99ddb2f534579766454e03
eliphrock <eliphrock@gmail.com>
2023-01-14 10:49:23 -0500

    invalid informations

--- IdeaProjects/testNGProject/src/test/java/techproed/tests/smoketest/logintests/Day19_NegativeLoginTest.java
+++ IdeaProjects/testNGProject/src/test/java/techproed/tests/smoketest/logintests/Day19_NegativeLoginTest.java
@@ -1,5 +1,6 @@
 package techproed.tests.smoketest.logintests;
 
+import com.github.javafaker.Faker;
 import junit.framework.Assert;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
@@ -9,30 +10,63 @@ import techproed.utilities.ConfigReader;
 import techproed.utilities.Driver;
 import techproed.utilities.ReusableMethods;
 
-public class Day19_NegativeLoginTest {
+import java.io.IOException;
 
+public class Day19_NegativeLoginTest {
     HomePage homePage;
-    LogInPage logInPage;
+    LogInPage loginPage;
+    Faker faker;
     @Test
-    public void US100208_Negative_Login(){
+    public void US100208_Negative_Login() throws IOException {
+//        As customer, I should not be able to log in the application
         //going blue rental car home page
         Driver.getDriver().get(ConfigReader.getProperty("app_home_url"));
 //        click on home page login link
         homePage = new HomePage();
-        logInPage= new LogInPage();
+        loginPage= new LogInPage();
         ReusableMethods.waitFor(3);
         homePage.homePageLoginLink.click();
 //        sending credentials and clicking on login button
         ReusableMethods.waitFor(3);
-        logInPage.userName.sendKeys("fake@bluerentalcars.com");
+//        Customer email: fake@bluerentalcars.com
+//        Customer password: fakepass
+        loginPage.userName.sendKeys("fake@bluerentalcars.com");
         ReusableMethods.waitFor(3);
-        logInPage.password.sendKeys("fakepass");
+        loginPage.password.sendKeys("fakepass");
         ReusableMethods.waitFor(3);
-        logInPage.loginButton.click();
+        loginPage.loginButton.click();
         ReusableMethods.waitFor(3);
-        //verify error is successful
-        String errorMessage=logInPage.errorMessage.getText();
+//        Error: User with email fake@bluerentalcars.com not found
+        String errorMessage = loginPage.errorMessage.getText();
         Assert.assertEquals(errorMessage,"User with email fake@bluerentalcars.com not found");
-
+        ReusableMethods.getScreenshot("NegativeLoginScreenshot");
+    }
+    @Test
+    public void invalidCredsTest() throws IOException {
+//        As customer, I should not be able to log in the application
+        //going blue rental car home page
+        Driver.getDriver().get(ConfigReader.getProperty("app_home_url"));
+//        click on home page login link
+        homePage = new HomePage();
+        loginPage= new LogInPage();
+        ReusableMethods.waitFor(3);
+        homePage.homePageLoginLink.click();
+//        sending credentials and clicking on login button
+        ReusableMethods.waitFor(3);
+//        Customer email: fake@bluerentalcars.com
+//        Customer password: fakepass
+        faker=new Faker();
+        String fakeEmail = faker.internet().emailAddress();
+        loginPage.userName.sendKeys(fakeEmail);
+        ReusableMethods.waitFor(3);
+        loginPage.password.sendKeys(faker.internet().password(4,6));//fake pass 4-6 digit
+        ReusableMethods.waitFor(3);
+        loginPage.loginButton.click();
+        ReusableMethods.waitFor(3);
+//        Error: User with email fake@bluerentalcars.com not found
+        String errorMessage = loginPage.errorMessage.getText();
+        Assert.assertEquals(errorMessage,"User with email "+fakeEmail+" not found");
+        ReusableMethods.getScreenshot("NegativeLoginScreenshot");
+        Driver.closeDriver();
     }
 }
